79c2707619d39afe3bbaf618f266bff493a84856
plnttr1003 <top@nrdn.ru>
2018-02-18 22:36:46 +0300

    divide path

--- scripts.v2.js
+++ scripts.v2.js
@@ -1,11 +1,13 @@
 var data = {
 	const: {
-		seatDelta: 2
+		seatDelta: 2 // количество кресел между рядами
 	},
 	levelName: '',
 	betweenPlaces: [],
+	coordinates: {x:[], y:[]},
 	seatSize: 0,
-	seatsByLevel: []
+	seatsByLevel: [],
+	sectors: {}
 }
 
 var el = {
@@ -18,31 +20,35 @@ var main = {
 		console.log(hallplan);
 		hallplan.levels.forEach(function(level, i) {
 			//console.log('Level', level);
-			console.log('Level id', level.name, level.id, '\n------------------------------------------');
-			console.log('\n------------------------------------------');
-			console.log('seatsByLevel', main.seatsByLevel(level));
-			console.log('\n\n\n');
+			//console.log('Level id', level.name, level.id, '\n------------------------------------------');
+			//console.log('\n------------------------------------------');
+			//console.log('seatsByLevel', main.seatsByLevel(level));
+			//console.log('\n\n\n');
 			data.levelName = level.name;
 			data.seatsByLevel = main.seatsByLevel(level);
-			data.seatsByLevel.forEach(function(seats) {
-				main.pushSectorPoints(seats);
-			});
-			data.seatSize = Math.min.apply(null, data.betweenPlaces);
-			data.seatsByLevel.forEach(function(seats) {
-
-				//console.log('colors I ===', i, el.colors[i], i % el.colors.length);
-
-				main.drawPath(seats, el.colors[i]);
-				main.drawUses(seats, el.colors[i]);
-				main.setNumbers();
-				//main.dividePaths(); Разделить пути на части
-				//main.groupPathsBySectors(); Объединить по секторам
-				// Нужно будет каждую линию отофсеттить на ширину data.betweenPlaces.
-				// Разбить на блоки секторов. И посеторно сгруппировать
-			});
-			//main.drawVerticalPaths(data.seatsByLevel);
+			if (data.seatsByLevel) {
+				data.seatsByLevel.forEach(function(seats) {
+					main.pushSectorPoints(seats);
+				});
+				data.seatSize = Math.min.apply(null, data.betweenPlaces);
+				main.setViewPortSize();
+				var k = 1;
+				data.seatsByLevel.forEach(function(seats) {
+					main.drawPath(seats, el.colors[i % el.colors.length]);
+					//main.drawUses(seats, el.colors[i % el.colors.length]);
+					//main.dividePaths(); Разделить пути на части
+					//main.groupPathsBySectors(); Объединить по секторам
+					// Нужно будет каждую линию отофсеттить на ширину data.betweenPlaces.
+					// Разбить на блоки секторов. И посеторно сгруппировать
+				});
+				//main.drawVerticalPaths(data.seatsByLevel);
+			}
 		});
+		main.drawOutlines();
 		main.downloadSvg();
+	},
+	setViewPortSize: function() {
+		el.svg.setAttribute('viewBox', `${(Math.min.apply(null, data.coordinates.x) - data.seatSize)} ${(Math.min.apply(null, data.coordinates.y) - data.seatSize)} ${Math.max.apply(null, data.coordinates.x)} ${Math.max.apply(null, data.coordinates.y)}`);
 	},
 	pushSectorPoints(points) {
 		points.forEach(function(point, i) {
@@ -51,52 +57,111 @@ var main = {
 				data.tempPath.setAttribute('d', `M ${points[i-1][0]} ${points[i-1][1]} L ${point[0]} ${point[1]}`);
 				data.betweenPlaces.push(data.tempPath.getTotalLength());
 			}
+			data.coordinates.x.push(point[0]);
+			data.coordinates.y.push(point[1]);
 		});
 	},
 	seatsByLevel: function(level) {
 		var levelSeats = [];
-		level.rows.forEach(function(row) {
-			var rowSeats = [];
-			row.seats.forEach(function(seat, i) {
-				rowSeats.push([seat.x, seat.y]);
+		if (level.rows) {
+			level.rows.forEach(function(row) {
+				var rowSeats = [];
+				row.seats.forEach(function(seat, i) {
+					rowSeats.push([seat.x, seat.y]);
+				});
+				levelSeats.push(rowSeats);
 			});
-			levelSeats.push(rowSeats);
-		});
-		return levelSeats;
+			return levelSeats;
+		} else {
+			return false;
+		}
 	},
-	createPath: function(points, moveTo) { // создаем пути. Аргументы -- массив точек и флаг moveTo -- разрыв или целая линия
+	createPath: function(points, moveTo, array, fn) { // создаем пути. Аргументы -- массив точек и флаг moveTo -- разрыв или целая линия
+		var index = 0;
 		var path = '';
+		if (!fn) {
+			var pathArr = [];
+		}
 		var plus = false;
 		moveTo.map(function(status, i) {
 			if (status) {
-				path += `M ${points[i][0]} ${points[i][1]} L ${points[i + 1][0]} ${points[i + 1][1]}`;
+				path += `${plus ? '' : 'M ' + points[i][0] + ' ' + points[i][1]} L ${points[i + 1][0]} ${points[i + 1][1]}`;
 				plus = true;
 			} else {
+				index += 1;
 				path += `M ${points[plus ? i + 1 : i][0]} ${points[plus ? i + 1 : i][1]} L ${points[plus ? i + 1 : i][0]} ${points[plus ? i + 1 : i][1]}`;
 				plus = false;
 			}
 		});
-		return path;
+		if (!fn) {
+			pathArr = path.split('M ').slice(1).map(function(item) {
+				return 'M ' + item;
+			});
+			//console.log('pathArr', pathArr); // dividedPATH
+		}
+		return array ? pathArr : path;
 	},
 	drawPath: function(points, color, fn) {
-		var pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		var path;
+		var paths = [];
 		var moveTo = [];
 		if (Array.isArray(points)) {
-			path = [];
+			var pathArr = [];
 			points.forEach(function(point, i) {
 				if (i < points.length - 1) {
 					data.tempPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 					data.tempPath.setAttribute('d', `M ${point[0]} ${point[1]} L ${points[i + 1][0]} ${points[i + 1][1]}`);
-					moveTo.push(data.tempPath.getTotalLength() < data.seatSize * data.const.seatDelta);
+					moveTo.push(data.tempPath.getTotalLength() < data.seatSize * data.const.seatDelta); // заполняем для проверки статуса
 				}
-				path.push(point);
-				//path += `${i === 0 ? 'M' : 'L'} ${point[0]} ${point[1]}`;
+				pathArr.push(point);
 			});
+			if (!fn) {
+				paths = main.createPath(pathArr, moveTo, true, fn);
+			} else {
+				paths.push(main.createPath(pathArr, moveTo, false, fn));
+			}
 		} else {
-			path = points;
+			paths.push(points);
 		}
-		pathEl.setAttribute('d', main.createPath(path, moveTo));
+		if (fn) {
+			paths.forEach(function(path){
+				main.renderPath(path, color, fn);
+			})
+		} else {
+			main.combinePaths(paths);
+		}
+	},
+	combinePaths: function(paths) {
+		if (!data.sectors[data.levelName]) {
+			data.sectors[data.levelName] = {};
+		}
+		if (!data.sectors[data.levelName][paths.length]) {
+			data.sectors[data.levelName][paths.length] = {};
+		}
+		paths.forEach(function(path, i) {
+			if (!data.sectors[data.levelName][paths.length][i]) {
+				data.sectors[data.levelName][paths.length][i] = {paths:[]}
+			}
+			data.sectors[data.levelName][paths.length][i].paths.push(path);
+		})
+	},
+	drawOutlines: function() {
+		console.log('DATA SECTORS ===', data.sectors);
+		Object.keys(data.sectors).forEach(function(sector) {
+			console.log(sector, data.sectors[sector]);
+			Object.keys(data.sectors[sector]).forEach(function(group) {
+				//console.log(data.sectors[sector][group]);
+				Object.keys(data.sectors[sector][group]).forEach(function(rows, i) {
+					console.log(rows, data.sectors[sector][group][rows]);
+					data.sectors[sector][group][rows].paths.forEach(function(path) {
+						main.renderPath(path, el.colors[i % el.colors.length])
+					})
+				})
+			})
+		});
+	},
+	renderPath: function(path, color, fn) {
+		var pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		pathEl.setAttribute('d', path);
 		pathEl.setAttribute('level-name', data.levelName);
 		if (color) {
 			pathEl.setAttribute('style', `fill:none; stroke: ${color};`);
@@ -105,21 +170,15 @@ var main = {
 			el.svg.appendChild(pathEl);
 		//}
 		if (fn) {
-			//console.log('>>>', fn, pathEl);
 			fn(pathEl);
 		}
-	},
-	setNumbers: function() {
-
 	},
 	drawVerticalPaths: function(rows) {
 		var topPath = [];
 		var rightPath = [];
 		var bottomPath = [];
 		var leftPath = [];
-		//console.log('rows', rows);
 		rows.forEach(function(seats, k){
-			//console.log('seats', seats)
 			seats.forEach(function(seat, i) {
 				if (k === 0) {
 					topPath.push(seat);
@@ -169,10 +228,8 @@ var main = {
 						downloadLink.download = fileName;
 						downloadLink.innerHTML = 'Download File';
 						if ('webkitURL' in window) {
-							// Chrome allows the link to be clicked without actually adding it to the DOM.
 							downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
 						} else {
-							// Firefox requires the link to be added to the DOM before it can be clicked.
 							downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
 							downloadLink.onclick = destroyClickedElement;
 							downloadLink.style.display = 'none';
